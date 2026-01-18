5878ded58eff5737f66773c34895173e94e45cbd
tashfeen786 <tashfeenakhonzada786@gmail.com>
2026-01-18 22:16:14 +0500

    `Update dependencies and remove unused components`

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import Dashboard from '@/components/Dashboard'
+import UltraModernDashboard from '@/components/Dashboard'
 
 export default function Home() {
-  return <Dashboard />
+  return <UltraModernDashboard />
 }
